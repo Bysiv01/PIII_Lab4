7699a7233d7b0a6a8ba69726f8e1cfd433d1ef86
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-12-01 15:33:09 +0100

    init

--- PIII_Lab4/Program.cs
+++ PIII_Lab4/Program.cs
@@ -2,11 +2,66 @@
 
 namespace PIII_Lab4
 {
+
+
     class Program
     {
+
+        static void automatycznie_wypelnienie(Ksiazka[,,] Regal)
+        {
+            Ksiazka ksiazka = new Ksiazka("test", "test", 1, 1, 1);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Regal[i, j, k] = ksiazka;
+                    }
+                }
+            }
+
+            Ksiazka a = new Ksiazka("dziady", "Adam Mickiewicz", 2, 1, 3);
+
+            Regal[2, 1, 3] = a;
+
+        }
+
+        static void szukanie(Ksiazka[,,] Regal, string sprawdzanie)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+
+                        if (Regal[i, j, k].Autor.Contains(sprawdzanie, StringComparison.OrdinalIgnoreCase) || Regal[i, j, k].Tytul.Contains(sprawdzanie, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine($"Twoja książka znajduje się na półce: {Regal[i, j, k].NrPolki}, w regale: {Regal[i, j, k].NrRegalu} na miejscu: {Regal[i, j, k].NrMiejscaNaPolce}");
+                            return;
+                        }
+
+                    }
+
+                }
+            }
+            Console.WriteLine("nie można znaleźć książki");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Ksiazka[,,] Regal = new Ksiazka[3, 6, 10];
+
+            automatycznie_wypelnienie(Regal);
+
+            Console.WriteLine("Wpisz tytul lub autora szukanej książki ");
+            string sprawdzenie = Console.ReadLine();
+
+            szukanie(Regal, sprawdzenie);
         }
     }
 }
---
+++ PIII_Lab4/ksiazka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+
+        public int NrRegalu = 0;
+        public int NrPolki = 0;
+        public int NrMiejscaNaPolce = 0;
+
+
+        public Ksiazka(string tytul, string autor, int nrRegalu, int nrPolki, int nrMiejscaNaPolce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            NrRegalu = nrRegalu;
+            NrPolki = nrPolki;
+            NrMiejscaNaPolce = nrMiejscaNaPolce;
+        }
+
+    }
+
